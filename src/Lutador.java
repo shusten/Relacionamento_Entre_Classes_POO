c503453235861c018204d3f80dd29eae286d1e13
Luis Villas <shusten@hotmail.com>
2019-12-04 22:21:53 -0300

    First Commit

---
+++ src/Lutador.java
@@ -0,0 +1,12 @@
+public class Lutador {
+private String nome;
+private String nacionalidade;
+private int idade;
+private float altura;
+private float peso;
+private String categoria;
+private int vitorias;
+private int derrotas;
+private int empates;
+
+}
