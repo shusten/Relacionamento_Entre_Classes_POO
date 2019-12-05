6b6b18182a7226e8251be6b394b8e43043d4eeaa
Luis Villas <shusten@hotmail.com>
2019-12-05 00:26:59 -0300

    Vetor lutador

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        Lutador l[] = new Lutador[6];
+
+        l[0] = new Lutador("Pretty boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+
+        l[0].status();
+
+    }
+
+}
