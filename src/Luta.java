8d87737a004cb9e2378750beed33eb8252e2a6f3
Luis Villas <shusten@hotmail.com>
2019-12-04 23:21:07 -0300

    Add classe luta

---
+++ src/Luta.java
@@ -0,0 +1,15 @@
+public class Luta {
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    public void marcarLuta(){
+
+    }
+
+    public void lutar(){
+
+    }
+
+}
